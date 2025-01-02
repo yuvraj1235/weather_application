9d0e8ab8c7b56a1c9ad724df3000a066283e824f
yuvraj12345 <yuvrajspeedforce@gmail.com>
2025-01-02 22:01:23 +0530

    weather complt

--- script.js
+++ script.js
@@ -21,18 +21,25 @@ document.addEventListener("DOMContentLoaded",()=>{
   })
   async function fetchWeatherData(city){
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
-    const response=fetch(url)
+    const response=await fetch(url)
     if(!response.ok){
         throw new Error(" City Not found");
     }
     const data = await response.json();
     return data;
   }
-  function displayWeatherData(weatherData){
+  function displayWeatherData(data){
+      console.log(data);
+      const {name,main,weather}= data;      
+      cityNameDisplay.textContent=name;
+      temperatureDisplay.textContent = `Temperature : ${main.temp}`;
+      descriptionDisplay.textContent = `Weather : ${weather[0].description}`;
 
+      weatherInfo.classList.remove("hidden")
+      errorMessage.classList.add("hidden")
   }
   function showError(){
-    weatherInfo.classList.add("hidden");
-    errorMessage.classList.remove("hidden");
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
   }
 })
